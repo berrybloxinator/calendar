a920f1679cd98f81da3f95d5cc134e56f887c074
Brady Liechty <berrybloxinator@gmail.com>
2019-09-25 20:52:22 -0600

    Third update

--- action.js
+++ action.js
@@ -12,27 +12,38 @@ let day = dayNames[currentDate.getDay()];
 let date = currentDate.getDate();
 let year = currentDate.getFullYear();
 let monthNum = currentDate.getMonth();
-let newDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
 
 //https://www.geeksforgeeks.org/how-to-get-the-number-of-days-in-a-specified-month-using-javascript/
 function daysInMonth (month, year) {
     return new Date(year, month + 1, 0).getDate();
 }
 
+function removeElement(element) {
+    $(`${element}`).remove();
+}
+
+function resetGrayedOut() {
+    for (let i = 0; i < 42; i++) {
+        $(`#td${i}`).removeClass('grayedOut');
+    }
+}
+
 function populateCalendar(days) {
-    let tempDate = currentDate;
-    tempDate.setMonth(currentDate.getMonth() - 1);
-    console.log(tempDate);
+    let newDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
     let tempDay = newDate.getDay();
+    let tempDate = new Date(year, monthNum - 1, 1);
+    tempDate.setDate(daysInMonth(tempDate.getMonth(), tempDate.getFullYear()) - (tempDay - 1));
+    let tempGetDate = tempDate.getDate();
 
     for (let i = 0; i < tempDay; i++) {
-        $(`#td${i}`).html('test');
+        $(`#td${i}`).html(tempGetDate).addClass('grayedOut');
+        tempGetDate++;
     }
 
     for (let i = 0; i < days; i++) {
-        if (tempDay > 34) {
+        if (tempDay === 35) {
             $('#table-body').append(`
-                <tr>
+                <tr id='remove'>
                     <td id='td35'></td>
                     <td id='td36'></td>
                     <td id='td37'></td>
@@ -49,8 +60,10 @@ function populateCalendar(days) {
 
     let restOfDays = 42 - tempDay;
 
+    console.log(restOfDays, tempDay);
+
     for (let i = 0; i < restOfDays; i++) {
-        $(`#td${tempDay}`).html(i + 1);
+        $(`#td${tempDay}`).html(i + 1).addClass('grayedOut');
         tempDay++;
     }
 }
@@ -66,9 +79,19 @@ function load() {
 }
 
 function clickLeft() {
-    console.log('left');
+    currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1);
+    $('#month').html(`${monthNames[currentDate.getMonth()]} ${currentDate.getFullYear()}`);
+    removeElement('#remove');
+    resetGrayedOut();
+    let days = daysInMonth(currentDate.getMonth(), currentDate.getFullYear());
+    populateCalendar(days);
 }
 
 function clickRight() {
-    console.log('right');
+    currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1);
+    $('#month').html(`${monthNames[currentDate.getMonth()]} ${currentDate.getFullYear()}`);
+    removeElement('#remove');
+    resetGrayedOut();
+    let days = daysInMonth(currentDate.getMonth(), currentDate.getFullYear());
+    populateCalendar(days);
 }
